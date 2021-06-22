ceaffbc9f868a983745df85ccaff2941a2d7d749
lucasgomesoficial <lucasgomesbr@gmail.com>
2021-06-22 15:51:35 -0300

    n

--- src/page/App.js
+++ src/page/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import SectionSecond from '../components/sectionSecond/SectionSecond'
 import { Container, GlobalStyle } from './styled'
 
 export default class App extends React.Component {
@@ -7,7 +6,6 @@ export default class App extends React.Component {
 		return(
       <Container>
         <GlobalStyle/>
-        <SectionSecond />
       </Container>
 		)
 	}
