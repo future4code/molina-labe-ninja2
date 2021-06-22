f08c3323094ca69009932097704c31e461171a29
artagnancoder <stayhomer@outlook.com>
2021-06-22 18:00:28 -0300

    .

--- src/page/App.js
+++ src/page/App.js
@@ -1,24 +1,13 @@
 import React from 'react'
-import MainCadastro from '../components/main-cadastro/MainCadastro'
 import { Container, GlobalStyle } from './styled'
-import MainInstitucional from '../components/main-institucional/MainInstitucional'
-import Footer from '../components/footer/Footer'
-import ListaOrdenacao from '../components/lista-ordenacao/ListaOrdenacao'
 
 export default class App extends React.Component {
 	render(){
 		return(
-      	<Container>
+      <Container>
         <GlobalStyle/>
-
-			<MainInstitucional/>
-			{/* <MainCadastro/> */}
-			{/* <ListaOrdenacao/> */}
-			<Footer/>
-      	</Container>
-
+		
       </Container>
-
 		)
 	}
 }
