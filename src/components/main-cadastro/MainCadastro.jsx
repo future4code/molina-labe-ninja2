26ede4fbeb35073af27f3abcae82d533d4b80d21
artagnancoder <stayhomer@outlook.com>
2021-06-23 20:40:54 -0300

    Implementação dos inputs controlados no componente MainCadastro.jsx

--- src/components/main-cadastro/MainCadastro.jsx
+++ src/components/main-cadastro/MainCadastro.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import axios from 'axios'
+/* import {baseURL, headers} from '../services/api' */
 import {
   InputDescricao,
   MainContainer,
@@ -10,13 +12,57 @@ import {
 } from './styled'
 
 export default class MainCadastro extends React.Component {
+
+  state = {
+    inputTitle: '',
+    inputPrice: '',
+    inputDescription: '',
+    /* inputPaymentMethods: [], */ //Pesquisar como colocar no value do select
+    inputDueDate: ''
+  }
+
+  
+
+  onChangeInputServico = (e) => {
+    this.setState({inputTitle: e.target.value})
+
+  }
+
+  onChangeInputValor = (e) => {
+    this.setState({inputPrice: e.target.value})
+  }
+
+  onChangeInputDescricao = (e) => {
+    this.setState({inputDescription: e.target.value})
+  }
+
+  onChangePrazo = (e) => {
+    this.setState({inputDueDate: e.target.value})
+  }
+/* 
+  createJob = () => {
+    axios.post(baseURL, headers) 
+    
+    .then() 
+    
+    .catch()
+
+  } */
+
   render() {
+
+
     return (
       <MainContainer>
         <h4>
           Nome do Serviço
         </h4>
-        <InputServico placeholder="Nome do serviço" />
+        <InputServico 
+          placeholder="Nome do serviço"
+          value={this.state.inputTitle}
+          onChange={this.onChangeInputServico}
+        
+        />
         <ContainerPagamento>
           <h4>Método de pagamento</h4>
           <SelectEstilizado>
@@ -26,12 +72,25 @@ export default class MainCadastro extends React.Component {
             <option>Boleto</option>
           </SelectEstilizado>
           <h4>Valor</h4>
-          <input placeholder="Valor" />
+          <input 
+            placeholder="Valor" 
+            value={this.state.inputPrice}
+            onChange={this.onChangeInputValor}
+
+          />
         </ContainerPagamento>
         <h4>Prazo</h4>
-        <InputDataEstilizado id="date" type="date" value="2017-06-01" />
+        <InputDataEstilizado 
+        id="date" 
+        type="date" 
+        value={this.state.inputDueDate}
+        onChange={this.onChangePrazo}
+        />
         <h4>Descrição do serviço:</h4>
-        <InputDescricao />
+        <InputDescricao 
+          value={this.state.inputDescription}
+          onChange={this.onChangeInputDescricao}
+        />
         <BotaoOferecerServico>Oferecer serviço!</BotaoOferecerServico>
         {/*Botão acima é sugestão de implementação*/}
       </MainContainer>
