a85f7f076213ae0ee66840d0cba459ddac0421c6
joaopedrocoder <joaopedrolr.7@gmail.com>
2021-06-24 00:38:34 -0300

    resolucao de conflitos

--- src/components/cards-servicos/CardsServicos.jsx
+++ src/components/cards-servicos/CardsServicos.jsx
@@ -7,7 +7,6 @@ export default class CardsServicos extends React.Component {
 		return(
 			<Container>
         <Title>
-<<<<<<< HEAD
           <h2>{this.props.titulo}</h2>
         </Title>
         <Description>
@@ -15,17 +14,6 @@ export default class CardsServicos extends React.Component {
         </Description>
         <Price>
           <p>{this.props.preco}</p>
-=======
-          <h2>{this.props.service}</h2>
-          <p>Prazo: {this.props.date}</p>
-        </Title>
-        <Description>
-          <h3>Descrição do projeto</h3>
-          <p>{this.props.description}</p>
-        </Description>
-        <Price>
-          <p>Valor: R$ {this.props.price},00</p>
->>>>>>> master
           <div>
             {this.props.paymentMethods.map((method) => {
               return (
